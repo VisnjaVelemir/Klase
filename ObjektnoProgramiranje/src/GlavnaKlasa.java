8bd209fc9528ba7e80619a21a69af76c1130650b
Visnja <visnjax@gmail.com>
2019-10-29 21:52:32 +0100

    Metode

---
+++ ObjektnoProgramiranje/src/GlavnaKlasa.java
@@ -0,0 +1,74 @@
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+		
+		
+		
+/*		Alat tool = new Alat ();
+		
+		tool.pozdrav(); 
+
+
+		tool.pozdrav (" Mase" );
+		tool.pozdrav (" medved ");
+
+		
+		String i = "Pera";
+		String p = " Prezime";
+		int g = 15;
+		
+
+		String name = tool.imeIprezime( i, p);
+		System.out.println ( name );
+		
+		
+		String brojGodina = tool.godine (g);
+		
+		System.out.println( brojGodina);
+		
+		
+		
+		
+		int prviBroj = 5;
+		int drugiBroj = 9;
+		
+		
+		
+		
+		int zbirBrojeva = tool.saberi(prviBroj, drugiBroj);
+		
+		System.out.println( "Zbir brojeva je " + zbirBrojeva);
+		*/
+
+
+		
+		
+/*
+	double x = Alat.kvadratBroja(7);
+	System.out.println(x);*/
+
+
+
+	
+	Drzava drzava = new Drzava ();
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
